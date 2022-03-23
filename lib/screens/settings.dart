3fd4dc3045f1119c3c3b2a53c5ec553d49f35f69
era-rapira <kol6asa.d@gmail.com>
2022-03-23 21:14:20 +0600

    settings ui

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -71,15 +71,392 @@ class _SettingsState extends State<Settings> {
               ),
             ),
             const SizedBox(height: 8,),
-            const Divider(
-              thickness: 1,
-              color: Color(0xFF3B3750),
-            ),
             Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: const Color(0xFF3B3750).withOpacity(0.3),
+                    width: 1,
+                  )
+                ),
+                color: Colors.white,
+              ),
               padding: const EdgeInsets.symmetric(vertical: 13),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Currency', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 11.0),
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 11.0),
+                          child: Text('USD', style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF65BCBF),
+                          ),),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: const Color(0xFF3B3750).withOpacity(0.3),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-            )
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                      color: const Color(0xFF3B3750).withOpacity(0.3),
+                      width: 1,
+                    )
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Appearance', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 11.0),
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 11.0),
+                          child: Text('USD', style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF65BCBF),
+                          ),),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: const Color(0xFF3B3750).withOpacity(0.3),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                      color: const Color(0xFF3B3750).withOpacity(0.3),
+                      width: 1,
+                    ),
+                  bottom: BorderSide(
+                    color: const Color(0xFF3B3750).withOpacity(0.3),
+                    width: 1,
+                  )
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Additionally', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 11.0),
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 11.0),
+                          child: Text('USD', style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF65BCBF),
+                          ),),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: const Color(0xFF3B3750).withOpacity(0.3),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 24,),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text('Premium',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.3)
+                ),
+              ),
+            ),
+            const SizedBox(height: 8,),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                      color: const Color(0xFF3B3750).withOpacity(0.3),
+                      width: 1,
+                    )
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Icon Set', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF65BCBF),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: const Text(
+                        'PRO',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                      color: const Color(0xFF3B3750).withOpacity(0.3),
+                      width: 1,
+                    )
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Backups', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF65BCBF),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: const Text(
+                        'PRO',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                      color: const Color(0xFF3B3750).withOpacity(0.3),
+                      width: 1,
+                    )
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Export', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF65BCBF),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: const Text(
+                        'PRO',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                      color: const Color(0xFF3B3750).withOpacity(0.3),
+                      width: 1,
+                    ),
+                  bottom: BorderSide(
+                    color: const Color(0xFF3B3750).withOpacity(0.3),
+                    width: 1,
+                  )
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Security', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF65BCBF),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: const Text(
+                        'PRO',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xFF65BCBF),
+                  ),
+                  onPressed: () {
+
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                    child: Text(
+                      'Subscribe',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 92,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                  ),
+                  onPressed: () {
+
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                    child: Text(
+                      'Send your review',
+                      style: TextStyle(
+                        color: Color(0xFF65BCBF),
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                  ),
+                  onPressed: () {
+
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                    child: Text(
+                      'Rate in App Store ',
+                      style: TextStyle(
+                        color: Color(0xFF65BCBF),
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
